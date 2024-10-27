ed43398b657b438c5156222a936932680d485096
Florian <mrminecrafttv19@gmail.com>
2024-10-27 08:43:57 +0100

    feat: added more texts

--- lib/texts.ts
+++ lib/texts.ts
@@ -31,5 +31,18 @@ export const MESSAGES = {
 		FEATURED_PROJECTS_DESC: "I have worked on many projects over the course of being a Web Developer, here are a few of my live, real-world projects",
 		CONNECT_WITH_ME: "Connect with me:",
 		CONNECT_WITH_ME_DESC: "Satisfied with my work? Lets get in touch and discuss your project."
-	}
+	},
+
+	EMAIL_TEMPLATE: {
+		NEW_CONTACT_FORM_SUBMISSION: "New Contact Form Submission",
+		FROM: "From: ",
+		EMAIL: "Email: ",
+		THIS_MESSAGE_WAS_SENT_FROM_YOUR_PORTFOLIO_CONTACT_FORM: "This message was sent from your portfolio contact form."
+	},
+
+	PROJECT_CARD: {
+		VIEW_PROJECT: "View project",
+		GITHUB_REPO: "Github Repo"
+	},
+
 }
